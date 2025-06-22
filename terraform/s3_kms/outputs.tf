c9ccfcf6c8338dea4b3977a1e4c0c58f2a986e61
Mariano Centeno <marianogabrielcenteno@gmail.com>
2025-06-22 03:54:37 -0300

    output s3 bucket names

--- terraform/s3_kms/outputs.tf
+++ terraform/s3_kms/outputs.tf
@@ -1,3 +1,10 @@
-output "bucket_name" {
-  value = aws_s3_bucket.force_and_lock_logs[each.key].bucket
+#output "bucket_name" {
+#  value = aws_s3_bucket.force_and_lock_logs[each.key].bucket
+#}
+
+output "bucket_names" {
+  value = {
+    for region, bucket in aws_s3_bucket.force_and_lock_logs :
+    region => bucket.bucket
+  }
 }
