c8733532da234705c95d47f1b18ca0102c140a03
Mariano Centeno <marianogabrielcenteno@gmail.com>
2025-06-23 11:51:06 -0300

    cf regional outputs.tf

--- terraform/cloudformation_regional/outputs.tf
+++ terraform/cloudformation_regional/outputs.tf
@@ -1,3 +1,7 @@
-output "global_stack_name" {
-  value = aws_cloudformation_stack.regional_resources_fall_root_org.name
+output "global_stack_name_primary" {
+  value = aws_cloudformation_stack.regional_resources_fall_root_org_primary.name
+}
+
+output "global_stack_name_secondary" {
+  value = aws_cloudformation_stack.regional_resources_fall_root_org_secondary.name
 }
